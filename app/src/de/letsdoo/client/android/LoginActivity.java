52d3cb991783a1f66e3858a8e3700751a381eee1
liese <liese@968d74b5-d3a8-4b1f-ab94-d6d52e350716>
2011-07-04 19:46:56 +0000

    implementing login

--- app/src/de/letsdoo/client/android/LoginActivity.java
+++ app/src/de/letsdoo/client/android/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import de.potpiejimmy.util.AsyncUITask;
 
 public class LoginActivity extends Activity implements OnClickListener {
 
@@ -28,4 +30,30 @@ public class LoginActivity extends Activity implements OnClickListener {
 		}
 	}
 
+	protected void login()
+	{
+		new LoginTask().go("Creating login credentials...");
+	}
+	
+	protected class LoginTask extends AsyncUITask<String>
+	{
+		public LoginTask() {
+			super(LoginActivity.this);
+		}
+
+		@Override
+		public String doTask() {
+			EditText email = (EditText) findViewById(R.id.email);
+
+			
+			return "xxx";
+		}
+
+		@Override
+		public void done(String result) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }

29b2c42be91d2d6d9dfdf6dfedfb7894e440a1ab
liese <liese@968d74b5-d3a8-4b1f-ab94-d6d52e350716>
2011-07-10 18:50:24 +0000

    fixed mail sending (authentication)

--- app/src/de/letsdoo/client/android/Letsdoo.java
+++ app/src/de/letsdoo/client/android/Letsdoo.java
@@ -16,7 +16,7 @@ public class Letsdoo extends Application {
 	public final static String PROTOSEC = "https://";
 	
 	public final static String URI = "www.potpiejimmy.de/doogetha/res/";
-	//public final static String URI = "192.168.100.30:8181/doogetha/res/";
+	//public final static String URI = "192.168.100.30:8080/doogetha/res/";
 	//public final static String URI = "192.168.100.22:8089/doogetha/res/";
 	//public final static String URI = "172.18.119.203:8089/doogetha/res/";
 	

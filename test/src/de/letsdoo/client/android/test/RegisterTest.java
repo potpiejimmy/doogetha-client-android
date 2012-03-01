ace9da2fac90748a954113d0408c5a455687d83d
wolfram <wolfram@968d74b5-d3a8-4b1f-ab94-d6d52e350716>
2012-03-01 20:47:27 +0000

    + exteded register test case

--- test/src/de/letsdoo/client/android/test/RegisterTest.java
+++ test/src/de/letsdoo/client/android/test/RegisterTest.java
@@ -1,16 +1,23 @@
 package de.letsdoo.client.android.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
 
 import com.jayway.android.robotium.solo.Solo;
 
+import de.letsdoo.client.android.EventsActivity;
 import de.letsdoo.client.android.WelcomeActivity;
+import de.letsdoo.client.android.R;
 
 public class RegisterTest extends
 		ActivityInstrumentationTestCase2<WelcomeActivity> {
 
 	private Solo solo; 
 	private WelcomeActivity mActivity;
+	private String fortfahren;
+	private String registrieren;
+	private String login;
+	private String unregister;
 //	private Button registerbutton;
 //	private Button loginbutton;
 //	private EditText email;
@@ -24,6 +31,10 @@ public class RegisterTest extends
 		super.setUp();
         solo = new Solo(getInstrumentation(), getActivity()); 
         mActivity = getActivity();
+        fortfahren = mActivity.getString(R.string.welcome_continue);
+        registrieren = mActivity.getString(R.string.register);
+        login = mActivity.getString(R.string.login);
+        unregister = mActivity.getString(R.string.unregister);
 //    	registerbutton = (Button) mActivity.findViewById(R.id.registerbutton);
 //    	loginbutton = (Button) mActivity.findViewById(R.id.loginbutton);
 //		email = (EditText) mActivity.findViewById(R.id.email);
@@ -36,6 +47,21 @@ public class RegisterTest extends
 //		 solo.clickOnButton("Registrieren");
 //		 solo.clickOnButton("Login");
 //	 }
+	 public void testRegister()
+	 {
+		 if (solo.getCurrentActivity().getClass() ==  EventsActivity.class)
+		 {
+			 solo.sendKey(KeyEvent.KEYCODE_MENU);
+			 solo.clickOnText(unregister);
+		 }
+		 solo.assertCurrentActivity("Wrong Activity", WelcomeActivity.class);
+		 solo.clickOnButton(fortfahren);
+	     solo.enterText(0, "wolfram.liese@t-online.de");
+		 solo.clickOnButton(registrieren);
+	     solo.sleep(60000);  // 60 seconds time to confirm via mail link manually ;-)
+		 solo.clickOnButton(login);
+		 solo.assertCurrentActivity("LOGIN FAILED", EventsActivity.class);
+	 }
 	 	
 //	public void _testUIStatesWithToken()
 //	{

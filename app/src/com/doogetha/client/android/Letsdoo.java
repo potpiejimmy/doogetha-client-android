edd1171104a25b2c69e1043e19ad8af6f602054a
liese <liese@968d74b5-d3a8-4b1f-ab94-d6d52e350716>
2012-09-21 14:43:03 +0000

    Server + iOS: Implemented Apple Push Notifications

--- app/src/com/doogetha/client/android/Letsdoo.java
+++ app/src/com/doogetha/client/android/Letsdoo.java
@@ -20,12 +20,12 @@ public class Letsdoo extends Application {
 	public final static String PROTO    = "http://";
 	public final static String PROTOSEC = "https://";
 	
-	public final static String URI = "www.potpiejimmy.de/doogetha/res/";
-	//public final static String URI = "192.168.178.21:8080/doogetha/res/";
-	//public final static String URI = "192.168.100.22:8089/doogetha/res/";
-	//public final static String URI = "172.18.119.203:8089/doogetha/res/";
+	public final static String URI = "www.doogetha.com/beta/res/";
+	//public final static String URI = "192.168.178.21:8080/beta/res/";
+	//public final static String URI = "192.168.100.22:8089/beta/res/";
+	//public final static String URI = "172.18.119.203:8089/beta/res/";
 	
-	public final static String DOWNLOADURL = "http://www.potpiejimmy.de/download/Doogetha.apk";
+	public final static String DOWNLOADURL = "http://www.doogetha.com/download/Doogetha.apk";
 	
 	public final static String GCM_SENDER_ID = "273459076205";
 	
